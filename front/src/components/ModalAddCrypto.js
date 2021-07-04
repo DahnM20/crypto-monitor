94e43587a7b16115b0b4bba43f6a1b5c8510d197
Damien Massol <damien.massol@etu.univ-amu.fr>
2021-07-04 08:04:40 +0200

    UpdateQuantityForm

--- front/src/components/ModalAddCrypto.js
+++ front/src/components/ModalAddCrypto.js
@@ -3,12 +3,30 @@ import '../styles/ModalAddCrypto.css'
 import FormAddCrypto from './FormAddCrypto.js'
 import { useState } from 'react'
 import { server } from '../assets/env.js'
+import swal from 'sweetalert';
 
 function ModalAddCrypto({show, handleClose}){
 
     const [name, updateName] = useState('')
     const [id, updateId] = useState('')
     const [quantity, updateQuantity] = useState(0)
+
+    function manageAlert(status){
+        if(status){
+          swal(`${name} a été ajouté !`, {
+              buttons: false,
+              icon: 'success',
+              timer: 1500,
+          });
+          handleClose()
+        } else {
+          swal(`Erreur lors de l'envoi, veuillez réessayer`, {
+              buttons: false,
+              icon: 'error',
+              timer: 1500,
+          });
+      }
+    }
 
     async function handleSubmit(){
       const body = { 'name' : name, 'id' : id, 'quantity' : quantity }
@@ -21,9 +39,9 @@ function ModalAddCrypto({show, handleClose}){
 
         const response = await fetch(`http://${server.host}:${server.port}/wallet`, requestOptions)
         const data = await response.json()
-        console.log('DATA : ' + data)
-        
-        handleClose()
+
+        manageAlert(data.result.ok);
+      
     }
 
     return (
