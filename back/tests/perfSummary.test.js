395534bb686e6240129212d4e02d20dd3efaecae
Dami246 <damienmassol@hotmail.fr>
2022-04-17 16:19:41 +0200

    Update perfSummary tests

--- back/tests/perfSummary.test.js
+++ back/tests/perfSummary.test.js
@@ -12,8 +12,7 @@ test('perf should be persisted', async () => {
         asset: 'ethereum',
         week: '2022-03-14',
         value: 11,
-        kind: 'wUSD',
-        id: 1
+        kind: 'vsUSD'
     }
 
     await new PerfSummary(perfOne).save();
@@ -29,8 +28,7 @@ test('perf with incorrect kind should not be persisted', async () => {
         asset: 'ethereum',
         week: '2022-03-14',
         value: 11,
-        kind: 'incorrect',
-        id: 1
+        kind: 'incorrect'
     }
 
     try { 
@@ -43,22 +41,20 @@ test('perf with incorrect kind should not be persisted', async () => {
     }
 })
 
-test('Asset creation should create new perfSummary', async () => {
+test('getAllSummaryForAsset should return an object with date field', async () => {
 
     const perfOne = {
         asset: 'ethereum',
         week: '2022-03-14',
         value: 11,
-        kind: 'wUSD',
-        id: 1
+        kind: 'vsUSD'
     }
 
     const perfTwo = {
         asset: 'ethereum',
         week: '2022-03-20',
         value: 22,
-        kind: 'wUSD',
-        id: 2
+        kind: 'vsUSD'
     }
 
     const expected = {
@@ -68,7 +64,36 @@ test('Asset creation should create new perfSummary', async () => {
     await new PerfSummary(perfOne).save()
     await new PerfSummary(perfTwo).save()
 
-    const perfSummary = await PerfSummary.getAllSummaryForAsset('ethereum', '2');
+    const perfSummary = await PerfSummary.getAllSummaryForAsset('ethereum', 'vsUSD', 2);
+
+    expect(perfSummary).not.toBeNull()
+    expect(perfSummary).toStrictEqual(expected)
+})
+
+test('getAllSummaryForAsset should return an object with date in correct order', async () => {
+
+    const perfOne = {
+        asset: 'ethereum',
+        week: '2022-03-30',
+        value: 11,
+        kind: 'vsUSD'
+    }
+
+    const perfTwo = {
+        asset: 'ethereum',
+        week: '2022-03-10',
+        value: 22,
+        kind: 'vsUSD'
+    }
+
+    const expected = {
+        asset: 'ethereum', '2022-03-10': 22, '2022-03-30' : 11
+    }
+
+    await new PerfSummary(perfOne).save()
+    await new PerfSummary(perfTwo).save()
+
+    const perfSummary = await PerfSummary.getAllSummaryForAsset('ethereum', 'vsUSD', 2);
 
     expect(perfSummary).not.toBeNull()
     expect(perfSummary).toStrictEqual(expected)
